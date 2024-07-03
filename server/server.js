aa76de3b9e9530f95a2cccc01f4328764134068e
Santhosh <santhosh.s@vantiva.com>
2024-07-03 09:48:45 +0530

    Line altered

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ connectDb();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1/users", baseRouter);
 app.use(errorHandler);
+app.use("/api/v1/users", baseRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
